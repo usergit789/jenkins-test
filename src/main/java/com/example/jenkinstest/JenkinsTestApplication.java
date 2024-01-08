7fe79b7b3ed647a6f1bcde6a4a69cd80a26314d8
usergit789 <ptucakovic18@gmail.com>
2024-01-08 23:32:36 +0100

    added git demo project to local repo

---
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,22 @@
+package com.example.jenkinstest;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class JenkinsTestApplication {
+
+	@PostConstruct
+	public void init() {
+		log.info("application started...");
+	}
+
+	public static void main(String[] args) {
+		log.info("application executed...");
+		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.jenkinstest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Slf4j
+class JenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.info("test case executing...");
+		assertEquals(true, true);
+	}
+
+}
