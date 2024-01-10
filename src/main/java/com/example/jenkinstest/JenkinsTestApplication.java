337fb78f75bc64e53bfeb8e715761bb978fafc2a
usergit789 <ptucakovic18@gmail.com>
2024-01-10 22:08:50 +0100

    added end point

--- src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -4,14 +4,22 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Slf4j
+@RestController
 public class JenkinsTestApplication {
 
 	@PostConstruct
 	public void init() {
 		log.info("application started!!!....");
+	}
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to javatechi";
 	}
 
 	public static void main(String[] args) {
